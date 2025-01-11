eeed7c54e1db1c2bde13ce26ac6be99d16a68704
SuadAtse <matheusdacostaalves@gmail.com>
2025-01-11 15:43:55 +0000

    Atualizando desafio Batalha naval para nivel mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,22 +9,80 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
-        {0, 0, 0, 0, 3, 0, 0, 3, 0, 0},
-        {0, 0, 0, 0, 3, 0, 0, 0, 3, 0},
-        {0, 0, 0, 0, 3, 0, 0, 0, 0, 3},
-        {0, 0, 0, 0, 3, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 3, 0, 3, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 3, 0},
-        {3, 3, 3, 3, 3, 0, 0, 0, 0, 3}
-    };
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++){
-            printf("A matris de posição [%d][%d] é = %s \n",i, j, (tabuleiro[i][j] == 3)?"Ocupado":"Livre");
+    int move;
+    int tabuleiro[3][5];
+    //Deixei em branco para que na hora de formar à figura não dar erro
+
+    printf("Qual habilidade você quer utilizar: \n");
+    printf("1 - Cone\n");
+    printf("2 - Octaedro\n");
+    printf("3 - Cruz\n");
+    printf("Escolha: ");
+    scanf("%d", &move);
+
+    switch (move)
+    {
+    case 1:
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                // Uso de condicional para fabricar a figura geométrica com mais facilidade
+                if (tabuleiro[i][j] ==  i == 0 && j ==  2 || (i == 1 && j ==  1) || (i == 1 && j ==  2) || (i == 1 && j ==  3) || (i == 2) ){
+                    tabuleiro[i][j] = 1;
+            }
+                else{
+                    tabuleiro[i][j] = 0;
+                }
+            }
         }
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                printf("%d", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+
+    case 2:
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                if (tabuleiro[i][j] ==  i == 0 && j ==  2 || (i == 1 && j ==  1) || (i == 1 && j ==  2) || (i == 1 && j ==  3) || (i == 2 && j == 2) ){
+                    tabuleiro[i][j] = 1;
+            }
+                else{
+                    tabuleiro[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                printf("%d", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+
+        case 3:
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                if (tabuleiro[i][j] ==  i == 0 && j ==  2 || (i == 1) || (i == 2 && j == 2) ){
+                    tabuleiro[i][j] = 1;
+            }
+                else{
+                    tabuleiro[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                printf("%d", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+
+    default:
+        printf("Erro. Tente novamente");
+        break;
     }
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
